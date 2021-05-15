d5c07ab2d50f4474f2686e75656bc3e93e9f1a4e
Filip Rybczyński <filipryba@gmail.com>
2021-05-15 22:20:24 +0200

    JS code for drum-kit.html; Day 1 completed

--- src/js/drum-kit.js
+++ src/js/drum-kit.js
@@ -1,3 +1,39 @@
 import '../scss/modules/drum-kit.scss';
 
 console.log("hello!");
+
+window.addEventListener('keydown', e => {
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) //specific value of the data-key property of audio tag
+
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) return; //stop the function from running
+    
+    audio.currentTime = 0; //rewind to the start, because if audio is already playing, play() method won't work unless audio is rewinded back to 0;
+    audio.play();
+
+    key.classList.add("playing");
+
+    // // Removing the class
+    // setTimeout(()=>{
+    //     key.classList.remove("playing");
+    // }, 70);
+
+    /**
+     *     this is a good solution, but that would require active synchronizing JS with CSS ('transform' parameter) in case the transform time is changed CSS... and vice versa. So could cause problems.
+     * 
+     * Better solution is to create an event listener checking if there is a transition that's ending (since the only transitions we're concerned with are those on the keys)
+     */
+
+})
+
+function removeEffect(e) {
+    if (e.propertyName !== 'transform') return; // skipping all transitions but one (there are several transitions happening: border, box shadow etc)
+
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeEffect)); // function can be declared separately and reference (not called!) to it added here
