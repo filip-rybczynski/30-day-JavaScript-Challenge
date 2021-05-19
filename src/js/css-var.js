aa7ceb5ae5c15b2c8f9153935b6a803b7628ee4d
Filip Rybczyński <filipryba@gmail.com>
2021-05-19 22:10:55 +0200

    Day 3: Added full JS, day completed

--- src/js/css-var.js
+++ src/js/css-var.js
@@ -1,1 +1,15 @@
 import "../scss/css-var.scss";
+
+const inputs = document.querySelectorAll('.editor__controls input');
+
+function handleUpdate() {
+const suffix = this.dataset.sizing || ''; // = "px" or '' (we don't want to append "undefined" when "data-sizing" parameter is absent)
+//dataset - object which contains all custom parameters which use the "data-" prefix 
+
+document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate)); // so that updates are dynamic
+
+console.log("Day 3 log: Nice exercise, although I was a bit rusty on how CSS variables (and inputs, for that matter) work and had to go through the video step-by-step to get it done. But thanks to that, really happy with how quick this one went, compared to last day's challenge 😁")
