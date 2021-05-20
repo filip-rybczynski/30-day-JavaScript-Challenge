ba3a39bd18301917fbba13f6834d14045095d8eb
Filip Rybczyński <filipryba@gmail.com>
2021-05-20 21:16:14 +0200

    Day 1: resolved bug with press effect getting stuck when key held too long

--- src/js/drum-kit.js
+++ src/js/drum-kit.js
@@ -1,39 +1,44 @@
-import '../scss/drum-kit.scss';
+import "../scss/drum-kit.scss";
 
-console.log("hello!");
+console.log("Day 1 log: This was fun! Learned quite a bit with this one, even managed to discover a bug and find a solution to fix it afterwards (previously, if you held a key for too long, the respective field would get stuck on the 'pressed' animation 😅");
 
-window.addEventListener('keydown', e => {
+window.addEventListener("keydown", (e) => {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //specific value of the data-key property of audio tag
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) //specific value of the data-key property of audio tag
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; //stop the function from running
 
-    if(!audio) return; //stop the function from running
-    
-    audio.currentTime = 0; //rewind to the start, because if audio is already playing, play() method won't work unless audio is rewinded back to 0;
-    audio.play();
+  audio.currentTime = 0; //rewind to the start, because if audio is already playing, play() method won't work unless audio is rewinded back to 0;
+  audio.play();
 
-    key.classList.add("playing");
+  key.classList.add("playing");
 
-    // // Removing the class
-    // setTimeout(()=>{
-    //     key.classList.remove("playing");
-    // }, 70);
+  // // Removing the class
+  // setTimeout(()=>{
+  //     key.classList.remove("playing");
+  // }, 70);
 
-    /**
-     *     this is a good solution, but that would require active synchronizing JS with CSS ('transform' parameter) in case the transform time is changed CSS... and vice versa. So could cause problems.
-     * 
-     * Better solution is to create an event listener checking if there is a transition that's ending (since the only transitions we're concerned with are those on the keys)
-     */
-
-})
+  /**
+   *     this is a good solution, but that would require active synchronizing JS with CSS ('transform' parameter) in case the transform time is changed CSS... and vice versa. So could cause problems.
+   *
+   * Better solution is to create an event listener checking if there is a transition that's ending (since the only transitions we're concerned with are those on the keys)
+   */
+});
 
 function removeEffect(e) {
-    if (e.propertyName !== 'transform') return; // skipping all transitions but one (there are several transitions happening: border, box shadow etc)
+  if (e.propertyName !== "transform") return; // skipping all transitions but one (there are several transitions happening: border, box shadow etc)
 
-    this.classList.remove("playing");
+  this.classList.remove("playing");
 }
 
-const keys = document.querySelectorAll('.key');
+function removeAllKeyEffects(e) {
+  console.log("hello!");
+  keys.forEach((key) => key.classList.remove("playing"));
+}
 
-keys.forEach(key => key.addEventListener('transitionend', removeEffect)); // function can be declared separately and reference (not called!) to it added here
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => key.addEventListener("transitionend", removeEffect)); // function can be declared separately and reference (not called!) to it added here
+
+window.addEventListener("keyup", removeAllKeyEffects); // function added to resolve bug where drum kit key pressed twice quickly or held for a longer time got stuck with the "playing" effect
