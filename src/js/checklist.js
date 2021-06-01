dc7dde3ebad6d23d206b4a8c0083e07885ff3fb0
Filip Rybczyński <filipryba@gmail.com>
2021-06-01 13:40:54 +0200

    Day 10: JS - final version without course video help

--- src/js/checklist.js
+++ src/js/checklist.js
@@ -1,59 +1,63 @@
 import "../scss/modules/checklist.scss";
 
-const checklist = document.querySelectorAll("input");
+const checklist = Array.from(document.querySelectorAll("input"));
 
 let shiftDown = false;
 
+let lastClickedIndex = -1;
+/**
+ * Sets to index of last box checked
+ *
+ * if Shift key is pressed with no boxes checked, it's set to -1 again and the index of the first box checked with Shift down is the new value
+ */
+
 function crossOut() {
-// console.dir(this.nextElementSibling);
-console.dir(this);
+  let currentIndex = checklist.findIndex((checkbox) => checkbox === this);
 
-if(shiftDown === true) {
-    const checkboxes = [...checklist];
-
-    const alreadyChecked = checkboxes.findIndex((checkbox, index) => index !== this.checkboxNo && checkbox.checked === true);
-
-    if(alreadyChecked < this.checkboxNo) {
-        checklist.forEach((item, index) => {
-            if (index > alreadyChecked && index < this.checkboxNo) {
-                item.checked = true;
-                item.nextElementSibling.classList.add("selected");
-            }
-        })
+  if (lastClickedIndex >= 0 && shiftDown === true) {
+    if (lastClickedIndex <= currentIndex) {
+      checklist.forEach((checkbox, index) => {
+        if (index >= lastClickedIndex && index <= currentIndex) {
+          checkbox.checked = true;
+          checkbox.nextElementSibling.classList.add("selected");
+        } else {
+          checkbox.checked = false;
+          checkbox.nextElementSibling.classList.remove("selected");
+        }
+      });
+    } else if (lastClickedIndex > currentIndex) {
+      checklist.forEach((checkbox, index) => {
+        if (index <= lastClickedIndex && index >= currentIndex) {
+          checkbox.checked = true;
+          checkbox.nextElementSibling.classList.add("selected");
+        } else {
+          checkbox.checked = false;
+          checkbox.nextElementSibling.classList.remove("selected");
+        }
+      });
     }
-    if (alreadyChecked > this.checkboxNo) {
-        checklist.forEach((item, index) => {
-            if (index < alreadyChecked && index > this.checkboxNo) {
-                item.checked = true;
-                item.nextElementSibling.classList.add("selected");
-            } 
-        })
-    }
-}
+    return;
+  }
 
-// if(shiftDown === false && this.checked === false) {
-//     checklist.forEach(item => {
-//         item.checked = false;
-//         item.nextElementSibling.classList.remove("selected");
-//     })
-// }
+  this.nextElementSibling.classList.toggle("selected");
 
-this.nextElementSibling.classList.toggle("selected");
-
-
+  lastClickedIndex = currentIndex;
 }
 
 checklist.forEach((input, index) => {
-    input.addEventListener("change", crossOut)
-    input.checkboxNo = index;    
+  input.addEventListener("change", crossOut);
+  input.checkboxNo = index;
 });
 
-window.addEventListener("keydown", function(e) {
-    if(e.key !== "Shift") return;
-    shiftDown = true;
-})
+window.addEventListener("keydown", function (e) {
+  if (e.key !== "Shift") return;
+  shiftDown = true;
 
-window.addEventListener("keyup", function(e) {
-    if(e.key !== "Shift") return;
-    shiftDown = false;
-})
+  if (checklist.every((checkbox) => checkbox.checked === false))
+    lastClickedIndex = -1;
+});
+
+window.addEventListener("keyup", function (e) {
+  if (e.key !== "Shift") return;
+  shiftDown = false;
+});
