08d3d49648822ee17e4b9d2a381525483636b7f0
Filip Rybczyński <filipryba@gmail.com>
2021-06-14 19:24:11 +0200

    Day 15: final JS code amendments + remove item option added - challenge completed

--- src/js/tapas-menu.js
+++ src/js/tapas-menu.js
@@ -1,5 +1,9 @@
 import "../scss/modules/tapas-menu.scss";
 
+console.log(
+  "Day 15 log: A challenge with a few twists. I was able to find a solution on my own and initially I completed the challenge myself. However the solution as described in the course video was much cleaner, so I did a few changes to the code. I also added the option of removing list items"
+);
+
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
 
@@ -33,6 +37,7 @@ function generateList(plates = [], listElement) {
               plate.checked ? "checked" : ""
             } " id="item${i}" data-index='${i}'/>
             <label for='item${i}'>${plate.text}</label>
+            <span class="delete-item" data-remove=${i}>❌</span>
         </li>
         `;
     })
@@ -50,22 +55,34 @@ function generateList(plates = [], listElement) {
 // }
 
 function toggleCheck(e) {
-    // MY INITIAL SOLUTION, WITHOUT EVENT DELEGATION - THE FUNCTION WAS CALLED NOT ON THE PARENT, BUT ON EACH PARTICULAR CHILD
-//   const index = this.dataset.index;
-//   items[index].checked = this.checked;
+  // MY INITIAL SOLUTION, WITHOUT EVENT DELEGATION - THE FUNCTION WAS CALLED NOT ON THE PARENT, BUT ON EACH PARTICULAR CHILD
+  //   const index = this.dataset.index;
+  //   items[index].checked = this.checked;
 
-//   const listedPlates = JSON.stringify(items);
+  //   const listedPlates = JSON.stringify(items);
 
-//   localStorage.setItem("plates", listedPlates);
-const el = e.target;
+  //   localStorage.setItem("plates", listedPlates);
+  const el = e.target;
 
-if(!el.matches('input')) return;
+  if (!el.matches("input")) return;
 
-const index = el.dataset.index;
-items[index].checked = el.checked;
+  const index = el.dataset.index;
+  items[index].checked = el.checked;
+
+  localStorage.setItem("plates", JSON.stringify(items));
+}
+
+function deleteItem(e) {
+  const el = e.target;
+
+  if (!el.matches("span")) return;
+
+  const index = el.dataset.remove;
+  items.splice(index, 1);
 
   localStorage.setItem("plates", JSON.stringify(items));
 
+  generateList(items, this);
 }
 
 // BELOW CODE REPLACED BY OR OPERATOR IN THE "items" ARRAY ASSIGNMENT
@@ -79,4 +96,5 @@ items[index].checked = el.checked;
 generateList(items, itemsList);
 
 addItems.addEventListener("submit", addItem);
-itemsList.addEventListener('click', toggleCheck);
+itemsList.addEventListener("click", toggleCheck);
+itemsList.addEventListener("click", deleteItem);
